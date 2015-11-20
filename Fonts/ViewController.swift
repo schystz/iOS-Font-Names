44aace307ce6b2275807d03800339f43b69e2d07
James Russell Orola <jrmorola@gmail.com>
2015-11-20 13:48:27 +0800

    Added tableview to show available font family names in iOS

--- Fonts/ViewController.swift
+++ Fonts/ViewController.swift
@@ -8,18 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var fonts = [String: [UIFont]]()
+    var fontFamilyNames = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for familyName in UIFont.familyNames() {
+            var fontList = [UIFont]()
+            for name in UIFont.fontNamesForFamilyName(familyName) {
+                fontList.append(UIFont(name: name, size: 20)!)
+            }
+            fontFamilyNames.append(familyName)
+            fonts[familyName] = fontList
+        }
+        
+        fontFamilyNames.sortInPlace()
+        
+        tableView.dataSource = self
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return fontFamilyNames.count
     }
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let familyName = fontFamilyNames[section]
+        if let fontList = fonts[familyName] {
+            return fontList.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return fontFamilyNames[section]
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        let familyName = fontFamilyNames[indexPath.section]
+        if let fontList = fonts[familyName] {
+            let font = fontList[indexPath.row]
+            cell.textLabel?.text = font.fontName
+            cell.textLabel?.font = font
+        }
+        
+        return cell
+    }
 
 }
 
